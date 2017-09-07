a0441903a6e6b37b024788d4adfd3d7d50521411
Dendy Suprihady <dendyliu666@yahoo.com>
2017-09-07 09:57:07 +0700

    init commit

--- tes.py
+++ tes.py
@@ -2,6 +2,8 @@ from __future__ import print_function, division
 import nltk
 import os
 import random
+import re
+import math
 from nltk import word_tokenize, WordNetLemmatizer
 from nltk.stem.porter import *
 from nltk.stem.lancaster import *
@@ -12,22 +14,45 @@ from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
 from sklearn.svm import SVC, NuSVC, LinearSVC
 from sklearn.metrics import confusion_matrix 
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 #Stoplist di bahasa inggris
 #Stoplist itu berisi kata-kata yang gk penting yang bakal di remove contoho (so, only, is ,a , etc)
 stoplist = stopwords.words('english')
 
+
+#Buat list isi semua text email dari file data
+def init_lists(folder):
+    a_list = []
+    file_list = os.listdir(folder)
+    for a_file in file_list:
+        f = open(folder + a_file, 'r', encoding = "ISO-8859-1")
+        a_list.append(f.read())
+    f.close()
+    return a_list
+
+#Fungsi buat ilangin stopword dari string
+def remove_stopword(tokens):
+	return {word for word in tokens if not word in stoplist}
+
+
 #Preprocess data 
-# 1.Tokenization buat kata jadi token ilangin tanda baca atau simbol"
-# 2.Stemming / Lemmatization buat kata jadi kata dasar (katanya yang lebih bagus lemmatize)
-# 3.Ubah semua kata jadi lowercase supaya kata yang sama dianggap satu fitur
+# 1.Ilangin semua kata yang panjangnya <=2
+# 2.Ilangin Punctuation /Special Character
+# 3.Tokenization buat kata - kata jadi token dipisah-pisah
+# 4.Ilangin stopword dari token (kata-kata yang gak penting kaya is ,a , the)
+# 5.Ubah semua kata jadi lowercase supaya kata yang sama dianggap satu fitur
+# 6.Stemming / Lemmatization buat kata jadi kata dasar (katanya yang lebih bagus lemmatize)
 def preprocess(sentence):
-	tokens = word_tokenize(sentence)
+	str_removed_small = re.sub(r'\b\w{1,2}\b', '', sentence)
+	str_removed_symbol = re.sub(r'[^a-zA-Z0-9 ]',r' ',str_removed_small)
+	tokens = word_tokenize(str_removed_symbol)
+	tokens_removed_stopword = remove_stopword(tokens)
 	lemmatizer = WordNetLemmatizer() #Ubah jadi kata dasar , bisa ganti kata contoh (better jadi good)
 	stemmerizer  = PorterStemmer() #Motong kata jadi kata dasar, bisa ganti kata Leaves jadi Leav,  bagusnya lebih cepet
 	#stemmerizer = LancasterStemmer()
-	#return [lemmatizer.lemmatize(word.lower()) for word in tokens]
-	return [stemmerizer.stem(word.lower()) for word in tokens]
+	return [lemmatizer.lemmatize(word.lower()) for word in tokens_removed_stopword]
+	#return [stemmerizer.stem(word.lower()) for word in tokens]
 
 #Extract features
 #1 Baca email
@@ -38,19 +63,9 @@ def preprocess(sentence):
 #   Cukup di data katanya gk usah diitung
 def get_features(text, is_bow):
     if  is_bow.lower() == 'y':
-        return {word: count for word, count in Counter(preprocess(text)).items() if not word in stoplist}
+        return {word: count for word, count in Counter(preprocess(text)).items()}
     else:
-        return {word: True for word in preprocess(text) if not word in stoplist}
-
-#Buat list isi text email
-def init_lists(folder):
-    a_list = []
-    file_list = os.listdir(folder)
-    for a_file in file_list:
-        f = open(folder + a_file, 'r', encoding = "ISO-8859-1")
-        a_list.append(f.read())
-    f.close()
-    return a_list
+        return {word: True for word in preprocess(text)}
 
 #Fungsi buat train data 
 #Katanya paling bagus Naive Bayes buat spam filter , tapi untuk variannya belom tau
@@ -83,9 +98,42 @@ def train(features, samples_proportion,classifier_choose):
 def evaluate(train_set, test_set, classifier):
     print ('Accuracy on the training set = ' + str(classify.accuracy(classifier, train_set)))
     print ('Accuracy of the test set = ' + str(classify.accuracy(classifier, test_set)))
- 
 
-   
+
+#Buat itung TF
+#Term frekuensi frekuensi kemunculan term dalam 1 email 
+def sublinear_term_frequency(term, tokenized_document):
+    count = tokenized_document.count(term)
+    if count == 0:
+        return 0
+    return 1 + math.log(count)
+
+#Buat itung IDF
+#Bobot kemunculan term dalam semua email
+def inverse_document_frequencies(tokenized_documents):
+    idf_values = {}
+    all_tokens_set = set([item for sublist in tokenized_documents for item in sublist])
+    for tkn in all_tokens_set:
+        contains_token = map(lambda doc: tkn in doc, tokenized_documents)
+        idf_values[tkn] = 1 + math.log(len(tokenized_documents)/(sum(contains_token)))
+    return idf_values
+
+#Fungsi buat pake TF-IDF
+#Maping semua token yang dah di preprocess jadi nilai angka bobot kemunculan
+#Conceptnya pake BOW bag of word cuma perhitungan bobotnya lebih akurat
+#Kalau dijalanin lama makanya datanya di potong jadi 500
+def tfidf(documents):
+    tokenized_documents = [preprocess(email) for email in documents[:500]]
+    idf = inverse_document_frequencies(tokenized_documents)
+    tfidf_documents = []
+    for document in tokenized_documents:
+        doc_tfidf = {}
+        for term in idf.keys():
+            tf = sublinear_term_frequency(term, document)
+            doc_tfidf[term]= tf * idf[term]
+        tfidf_documents.append(doc_tfidf)
+    return tfidf_documents
+
 if __name__ == "__main__":
 	#ASUMSI  = DATA EMAIL BUKAN DATA MENTAH YANG MENGANDUNG HTML TAG, TAPI UDAH BERUPA TEKS
 	#folder isi data email text yang  spam bisa ganti 6 folder
@@ -98,6 +146,13 @@ if __name__ == "__main__":
 	print ('Data size = ' + str(len(all_emails)) + ' emails')
 	is_bow = input('Using bow ? (Y/N) ')
 	all_features = [(get_features(email, is_bow), label) for (email, label) in all_emails]
+	# =============== BUAT JALANIN TD-IDF ==================================
+	#all_documents = [(email) for (email,label) in all_emails]
+	#all_labels = [(label) for(email,label) in all_emails]
+	#tfidf_documents = tfidf(all_documents)
+	#all_features = list(map(lambda x,y:(x,y),tfidf_documents,all_labels))
+	# =============== BUAT JALANIN TD-IDF ==================================
+	print(all_features[0])
 	print ('Collected ' + str(len(all_features)) + ' feature sets')
 	samples_proportion = float(input('Distributed number for split data train-test: '))
 	classifier_choose = int(input('Choose your classifier: '))
